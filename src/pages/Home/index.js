27e08e492c13d7dd2f130598801f6050b346dc94
Lucia Moyano <luciamoyano2@gmail.com>
2020-09-11 20:37:12 -0300

    Cambio en home con rutas por paises

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,29 +2,29 @@ import React from 'react';
 import logo from '../../../src/logo-meli.png';
 import './style.scss';
 import {Link} from 'react-router-dom';
-
-function Home(){
-    const countriesArr = [
-        "Argentina",
-        "Bolivia",
-        "Brasil",
-        "Chile",
-        "Colombia",
-        "Costa Rica",
-        "Dominicana",
-        "Ecuador",
-        "Guatemala",
-        "Honduras",
-        "México",
-        "Nicaragua",
-        "Panamá",
-        "Paraguay",
-        "Perú",
-        "Salvador",
-        "Uruguay",
-        "Venezuela"];
-
-    return (
+const countriesArr = [
+    {name: "Argentina", site_id: "MLA"},
+    {name: "Bolivia", site_id: "MBO"},
+    {name: "Brasil", site_id: ""},
+    {name: "Chile", site_id: ""},
+    {name: "Colombia", site_id: ""},
+    {name: "Costa Rica", site_id: ""},
+    {name: "Dominicana", site_id: ""},
+    {name: "Ecuador", site_id: ""},
+    {name: "Guatemala", site_id: ""},
+    {name: "Honduras", site_id: ""},
+    {name: "México", site_id: ""},
+    {name: "Nicaragua", site_id: ""},
+    {name: "Panamá", site_id: ""},
+    {name: "Paraguay", site_id: ""},
+    {name: "Perú", site_id: ""},
+    {name: "Salvador", site_id: ""},
+    {name: "Uruguay", site_id: ""},
+    {name: "Venezuela", site_id: ""}
+];
+class Home extends React.Component {
+    render () {
+        return (
         <>
         <div id="homeWrapper">
             <header>
@@ -33,13 +33,14 @@ function Home(){
             <ul className="ml-site-list">
                 {countriesArr.map((country,key) => {
                     return (
-                    <li className="ml-site-country" key={key}><Link to="/search">{country}</Link></li>
+                    <li className="ml-site-country" key={key}><Link to={`/search/${country.site_id}`}>{country.name}</Link></li>
                     )
                 })}
             </ul>
         </div>
         </>
-    )
+        )
+    }
 };
 
 export default Home;
