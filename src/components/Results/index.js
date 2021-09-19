009db3db011caee7e98daa3fb00b83f9a6f60bae
Lucia Moyano <luciamoyano2@gmail.com>
2021-09-19 14:30:21 -0300

    masonry, media queries

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -2,30 +2,43 @@ import React from "react";
 import Item from "../Item";
 import "./style.scss";
 
+import Masonry from "react-masonry-css";
+
 export default function Results({ itemList, searchParam }) {
-  console.log(itemList);
+  const breakpointMasonry = {
+    default: 4,
+    1200: 3,
+    850: 2,
+    600: 1,
+  };
   return (
     <div className="itemContainer">
       <ul className="itemList">
-        {itemList
-          .filter((value) => {
-            return value.title
-              .toLowerCase()
-              .includes(searchParam.toLowerCase());
-          })
-          .map((value, key) => {
-            const { id, title, thumbnail, price } = value;
-            return (
-              <li key={id ? id : key}>
-                <Item
-                  name={title}
-                  image={thumbnail}
-                  price={price.toLocaleString()}
-                  id={id}
-                />
-              </li>
-            );
-          })}
+        <Masonry
+          breakpointCols={breakpointMasonry}
+          className="my-masonry-grid"
+          columnClassName="my-masonry-grid_column"
+        >
+          {itemList
+            .filter((value) => {
+              return value.title
+                .toLowerCase()
+                .includes(searchParam.toLowerCase());
+            })
+            .map((value, key) => {
+              const { id, title, thumbnail, price } = value;
+              return (
+                <li key={id ? id : key}>
+                  <Item
+                    name={title}
+                    image={thumbnail}
+                    price={price.toLocaleString()}
+                    id={id}
+                  />
+                </li>
+              );
+            })}
+        </Masonry>
       </ul>
     </div>
   );
